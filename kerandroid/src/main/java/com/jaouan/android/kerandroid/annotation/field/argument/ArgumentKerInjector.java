33852bde8013fb0b6eed846916a666b2534893c1
Maxence J <m.jaouan@gmail.com>
2015-08-03 22:36:05 +0200

    Fix argument.

--- kerandroid/src/main/java/com/jaouan/android/kerandroid/annotation/field/argument/ArgumentKerInjector.java
+++ kerandroid/src/main/java/com/jaouan/android/kerandroid/annotation/field/argument/ArgumentKerInjector.java
@@ -5,118 +5,117 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.jaouan.android.kerandroid.annotation.AbstractKerInjector;
 
 import java.lang.reflect.Field;
 
 /**
- *
  * KerInjector for {@link Argument} annotation.
- * 
+ *
  * @author Maxence Jaouan
- * 
  */
 public class ArgumentKerInjector extends AbstractKerInjector {
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	@Override
-	public void inject(final Field field, final Fragment fragment, final Bundle savedInstanceState) throws Exception {
-		// - Inject argument.
-		this.injectArgument(field, fragment, fragment.getArguments());
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    @Override
+    public void inject(final Field field, final Fragment fragment, final Bundle savedInstanceState) throws Exception {
+        // - Inject argument.
+        this.injectArgument(field, fragment, fragment.getArguments());
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void inject(final Field field, final android.support.v4.app.Fragment fragment, final Bundle savedInstanceState) throws Exception {
-		// - Inject argument.
-		this.injectArgument(field, fragment, fragment.getArguments());
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void inject(final Field field, final android.support.v4.app.Fragment fragment, final Bundle savedInstanceState) throws Exception {
+        // - Inject argument.
+        this.injectArgument(field, fragment, fragment.getArguments());
+    }
 
-	/**
-	 * Inject argument.
-	 * 
-	 * @param field
-	 *            Field to inject.
-	 * @param object
-	 *            Input object.
-	 * @param arguments
-	 *            Arguments.
-	 */
-	private void injectArgument(final Field field, final Object object, final Bundle arguments) throws Exception {
-		// - Retrieve field's name.
-		final String fieldName = getFieldId(field);
+    /**
+     * Inject argument.
+     *
+     * @param field     Field to inject.
+     * @param object    Input object.
+     * @param arguments Arguments.
+     */
+    private void injectArgument(final Field field, final Object object, final Bundle arguments) throws Exception {
+        // - Retrieve field's name.
+        final String fieldName = getFieldId(field);
 
-		// - Set fieldName key's value to field.
-		if (!field.isAccessible()) {
-			field.setAccessible(true);
-		}
-		field.set(object, arguments.get(fieldName));
-	}
+        // - Set fieldName key's value to field.
+        if (!field.isAccessible()) {
+            field.setAccessible(true);
+        }
+        try {
+            field.set(object, arguments.get(fieldName));
+        } catch (final IllegalArgumentException illegalArgumentException) {
+            // Exception when setting a null value in a primitive.
+            Log.i("KerAndroid", "No value to inject in field : " + fieldName);
+        }
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public boolean isInjectable(final Activity activity, final Bundle savedInstanceState) {
-		return false;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isInjectable(final Activity activity, final Bundle savedInstanceState) {
+        return false;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public boolean isInjectable(final android.support.v4.app.Fragment fragment, final Bundle savedInstanceState) {
-		return true;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isInjectable(final android.support.v4.app.Fragment fragment, final Bundle savedInstanceState) {
+        return true;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public boolean isInjectable(final Fragment fragment, final Bundle savedInstanceState) {
-		return true;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isInjectable(final Fragment fragment, final Bundle savedInstanceState) {
+        return true;
+    }
 
-	/**
-	 * Get unique field id.
-	 *
-	 * @param field
-	 *            Field.
-	 * @return Unique field id.
-	 */
-	private String getFieldId(final Field field) {
-		String fieldId = getAnnotationValue(field);
-		if(fieldId == null || "" == fieldId){
-			fieldId = field.getName();
-		}
-		return fieldId;
-	}
+    /**
+     * Get unique field id.
+     *
+     * @param field Field.
+     * @return Unique field id.
+     */
+    private String getFieldId(final Field field) {
+        String fieldId = getAnnotationValue(field);
+        if (fieldId == null || "" == fieldId) {
+            fieldId = field.getName();
+        }
+        return fieldId;
+    }
 
-	/**
-	 * Get annotation value.
-	 *
-	 * @param field
-	 *            Field.
-	 * @return Annotation value.
-	 */
-	private String getAnnotationValue(final Field field) {
-		String value = (String) this.getFromCache(field);
-		if (null == value) {
-			// - Find view by id.
-			final Argument findViewById = field.getAnnotation(Argument.class);
-			value = findViewById.value();
-			this.putToCache(field, value);
+    /**
+     * Get annotation value.
+     *
+     * @param field Field.
+     * @return Annotation value.
+     */
+    private String getAnnotationValue(final Field field) {
+        String value = (String) this.getFromCache(field);
+        if (null == value) {
+            // - Find view by id.
+            final Argument findViewById = field.getAnnotation(Argument.class);
+            value = findViewById.value();
+            this.putToCache(field, value);
 
-			// - Enable field access.
-			field.setAccessible(true);
-		}
-		return value;
-	}
+            // - Enable field access.
+            field.setAccessible(true);
+        }
+        return value;
+    }
 
 }
